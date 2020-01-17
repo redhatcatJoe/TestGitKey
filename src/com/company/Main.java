1fe65c5eb198f100255c61ee206813e51178a1fc
redhatcatJoe <redhatcatjoe@gmail.com>
2020-01-18 00:31:22 +0500

    master commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println ("dssdffsdfdfdffdvdvdvvddvdvdfefevbfbf" );
+        System.out.println ("dssdffsdfdfdffdvdvdvvddvdvdfefevbfbfdvdv" );
 
 
 	// write your code here
