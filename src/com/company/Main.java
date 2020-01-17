5ee3f8863b3b5aeb6a260d3e1930f67cf7183c11
redhatcatJoe <redhatcatjoe@gmail.com>
2020-01-18 00:26:08 +0500

    master push

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println ("dssdffsdfdfdffdvdvdvvddvdvd" );
+        System.out.println ("dssdffsdfdfdffdvdvdvvddvdvdfefe" );
 
 
 	// write your code here
