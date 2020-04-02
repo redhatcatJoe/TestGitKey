252e60a95803056a98d17b1820ac4787eb94d144
redhatcatJoe <redhatcatjoe@gmail.com>
2020-04-02 18:51:29 +0500

    lame local commit

--- src/com/company/dvdv.java
+++ src/com/company/dvdv.java
@@ -4,6 +4,6 @@ public class dvdv {
     //ddddf
 
     public dvdv() {
-        System.out.println ("ffgeegeggdfvddfddvdfvdvdvc cdfddfv 7777777 " );
+        System.out.println ("ffgeegeggdfvddfddvdfvdvdvc cdfddfv 7777777 lame local " );
     }
 }
